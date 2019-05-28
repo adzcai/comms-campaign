89479c83001865dc4f3e42f08cee0257e27826a6
Alexander Cai <alexandercai@outlook.com>
2019-05-27 20:51:42 -0600

    added efficiency post and script to generate posts

---
+++ _posts/2019-05-27-transparency.md
@@ -0,0 +1,5 @@
+---
+layout: post
+title: Transparency
+date: 2019-05-28 10:00:00 -0600
+---
---
+++ new_post.py
@@ -0,0 +1,21 @@
+import sys, re, datetime
+
+assert len(sys.argv) > 1, "Please specify the name of your post!"
+
+now = datetime.datetime.now()
+name = " ".join(sys.argv[1:])
+file_name = now.strftime('%Y-%m-%d-') + re.sub(r"[-' ]", '-', name.lower())
+
+if not (file_name.endswith('.md') or file_name.endswith('.markdown') or file_name.endswith('.html')):
+  file_name += '.md'
+
+print(name)
+print(file_name)
+with open("./_posts/" + file_name, "w") as file:
+  file.write('\n'.join([
+    '---',
+    'layout: post',
+    'title: ' + name,
+    'date: ' + now.strftime("%Y-%m-%d %H:%M:%S -0600"),
+    '---'
+  ]))
